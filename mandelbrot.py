a643964a873907e891cf6bcefe413ac61bac569c
Ross Hill <rosshill@protonmail.com>
2016-11-29 23:35:02 -0500

    Better implementation of OOP

--- mandelbrot.py
+++ mandelbrot.py
@@ -8,7 +8,6 @@ class Mandelbrot(Frame):
     def __init__(self, xCenter, yCenter, delta, h, w, bailout, paletteSize,
                  parent):
         Frame.__init__(self, parent)
-        self.paletteSize = paletteSize
         self.h = h
         self.w = w
         self.xCenter = xCenter
@@ -19,14 +18,16 @@ class Mandelbrot(Frame):
         self.ymin = yCenter + delta
         self.ymax = yCenter - delta
         self.bailout = bailout
-        self.parent = parent
         self.c, self.z = complex(0, 0), complex(0, 0)
         self.zoomFactor = 0.2
+        self.paletteSize = paletteSize
+        self.palette = self.getPalette()
+        self.totalPixels = self.h * self.w
+
+        self.parent = parent
         self.parent.title("Mandelbrot")
         self.pack(fill=BOTH, expand=1)
         self.canvas = Canvas(self)
-        self.palette = self.getPalette(self.paletteSize)
-        self.totalPixels = self.h * self.w
         self.draw()
         parent.bind("<Button-1>", self.clickEvent)
 
@@ -51,18 +52,15 @@ class Mandelbrot(Frame):
                 seconds = int(time.time())
                 if seconds != oldSeconds:
                     oldSeconds = seconds
-                    completed = round((x*self.h)/self.totalPixels*100, 2)
+                    completed = round((x * self.h) / self.totalPixels * 100, 2)
                     print("{}% completed".format(completed))
                 escapeTime = self.getEscapeTime(complex(0, 0), self.setC(x, y))
                 if escapeTime[0]:
                     color = self.palette[escapeTime[1] % self.paletteSize]
-                    self.drawPixel(x, y, color, self.canvas)
+                    self.drawPixel(x, y, color)
                 else:
-                    self.drawPixel(x, y, '#000000', self.canvas)
+                    self.drawPixel(x, y, '#000000')
         self.canvas.pack(fill=BOTH, expand=1)
-
-    def clamp(self, x):
-        return max(0, min(x, 255))
 
     def setC(self, col, row):
         re = self.translate(col, 0, self.w, self.xmin, self.xmax)
@@ -76,8 +74,8 @@ class Mandelbrot(Frame):
         valueScaled = float(value - leftMin) / float(leftSpan)
         return rightMin + (valueScaled * rightSpan)
 
-    def drawPixel(self, x, y, color, canvas):
-        canvas.create_line(x, y, x+1, y, fill=color)
+    def drawPixel(self, x, y, color):
+        self.canvas.create_line(x, y, x+1, y, fill=color)
 
     def getEscapeTime(self, z, c):
         for i in range(self.bailout):
@@ -86,7 +84,7 @@ class Mandelbrot(Frame):
                 return True, i
         return False, self.bailout
 
-    def getPalette(self, paletteSize):
+    def getPalette(self):
         palette = []
         redb = 2 * math.pi / (random.randint(0, 128) + 128)
         redc = 256 * random.random()
@@ -94,19 +92,23 @@ class Mandelbrot(Frame):
         greenc = 256 * random.random()
         blueb = 2 * math.pi / (random.randint(0, 128) + 128)
         bluec = 256 * random.random()
-        for i in range(paletteSize):
+        for i in range(self.paletteSize):
             red = int(256 * (0.5 * math.sin(redb * i + redc) + 0.5))
             green = int(256 * (0.5 * math.sin(greenb * i + greenc) + 0.5))
             blue = int(256 * (0.5 * math.sin(blueb * i + bluec) + 0.5))
-            r, g, b = self.clamp(red), self.clamp(green), self.clamp(blue)
+            r, g, b = clamp(red), clamp(green), clamp(blue)
             palette.append("#{0:02x}{1:02x}{2:02x}".format(r, g, b))
         return palette
+
+
+def clamp(x):
+    return max(0, min(x, 255))
 
 
 def main():
     master = Tk()
     height = width = round(master.winfo_screenheight()*0.9)
-    ex = Mandelbrot(-0.7, 0, 1.7, height, width, 100, 256, master)
+    ex = Mandelbrot(-0.7, 0, 1.7, height, width, 90, 256, master)
     master.geometry("{}x{}".format(width, height))
     master.mainloop()
 
